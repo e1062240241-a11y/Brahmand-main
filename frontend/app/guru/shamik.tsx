ca191de99ad25fdd5c964f97bac16137e6aaf1f5
17Prat <prarhampatel769@.com>
2026-08-03 12:11:02 +0530

    fix: update image asset path from png to webp in shamik.tsx

--- frontend/app/guru/shamik.tsx
+++ frontend/app/guru/shamik.tsx
@@ -156,7 +156,7 @@ const FEATURED_VIDEOS = [
     category: 'Shiv Katha',
     duration: '1h 24m',
     views: '45K views',
-    image: require('../../assets/images/panditji.png'),
+    image: require('../../assets/images/panditji.webp'),
   },
   {
     id: 'v2',
@@ -165,7 +165,7 @@ const FEATURED_VIDEOS = [
     category: 'Astrology Wisdom',
     duration: '42m',
     views: '89K views',
-    image: require('../../assets/images/panchang_icon_3.png'),
+    image: require('../../assets/images/panchang_icon_3.webp'),
   },
   {
     id: 'v3',
